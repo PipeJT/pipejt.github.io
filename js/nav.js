84d946c3f05e1623273302306686be38a552c47a
PipeJT <pipesuckit@hotmail.com>
2017-07-28 20:12:53 -0400

    Update page v2.4

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $menu = $('#menu').find('ul').find('li');
+$(document).ready(function () {
+    $menu = $("#menu").find("ul").find("li");
 
     $menu.hover(function () {
         $(this).children('ul').stop();
@@ -7,5 +7,15 @@ $(document).ready(function(){
     }, function () {
         $(this).children('ul').stop();
         $(this).children('ul').slideUp();
+    });
+    
+    $("#showSide").click(function () {
+        if($("#moveBtn").hasClass("activeSide")){
+            $("#moveBtn").removeClass("activeSide");
+            $("#side").removeClass("sideShow");
+        }else{
+            $("#moveBtn").addClass("activeSide");
+            $("#side").addClass("sideShow");
+        };
     });
 });
