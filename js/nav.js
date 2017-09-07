5fe31026311a0182db1fe0b5b09b9d2cd4a0e040
PipeJT <pipesuckit@hotmail.com>
2017-09-07 19:20:13 -0300

    Work in progress

--- js/nav.js
+++ js/nav.js
@@ -9,6 +9,16 @@ $(document).ready(function () {
         $(this).children('ul').slideUp();
     });
     
+    $menuMobile = $("#menuMovil").find("ul").find("li");
+
+    $menuMobile.hover(function () {
+        $(this).children('ul').stop();
+        $(this).children('ul').slideDown();
+    }, function () {
+        $(this).children('ul').stop();
+        $(this).children('ul').slideUp();
+    });
+    
     $("#showSide").click(function () {
         if($("#moveBtn").hasClass("activeSide")){
             $("#moveBtn").removeClass("activeSide");
